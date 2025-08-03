4d74739c2f9ebb4879bd2605b41159dcf4d0f080
William0831 <williamwangg0831@gmail.com>
2025-08-03 21:41:37 +1000

    William P

---
+++ work_shop2/uploading/practice.py
@@ -0,0 +1,6 @@
+print("Welcome to Inch CM Converter")
+input_value = input("Please enter the value of Inch:")
+converted_value = float(input_value)*2.54
+# 34 inch = 86.36 cm
+print(input_value + "inch = " + str(float(input_value)*2.54) + " com")
+print(f"{input_value} inch = {converted_value} com")
