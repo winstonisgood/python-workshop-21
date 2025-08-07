0b2c32eb43fb1fe9283e314be56bb592851b5a12
Huansong Zeng <huansongzeng@Mac.modem>
2025-08-07 19:53:27 +1000

    winston calculate average

---
+++ work_shop2/uploading/calculate_average_winston.py
@@ -0,0 +1,17 @@
+while_check = True
+
+while while_check:
+    number = input("Please enter how many number you going to enter: ")
+
+    try:
+        number = int(number)
+        while_check = False #break
+    except:
+        print("The text you entered is not a number!")
+
+sum = 0
+for i in range(number):
+    value = input("please enter a number: ")
+    sum += float(value) # sum = sum + float(value)
+
+print(f"The average number is {sum / number}")
