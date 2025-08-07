ccf6b5a88cea9dc1ec6f988e71e41e2aa6112181
Huansong Zeng <huansongzeng@Mac.modem>
2025-08-07 19:26:33 +1000

    remove file

--- work_shop2/InchCmConverter.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to Inch CM Coverter")
-a = input("Please enter the value of Inch:")
-print(a)
-
-b=float(a)*2.54
-print(b)
-
-#print(a+"inch ="+str(b)+"cm")
-print(f"{a}inch = {b}cm")
--- work_shop2/practice_EK.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to Inch CM Converter")
-input_value = input("Please enter the value of Inch: ")
-converted_value = float(input_value) * 2.54  # Convert Inch to CM
-#34 inch = 86.36 cm
-print(f"{input_value} inch = {converted_value} cm")  # Output the result
-
