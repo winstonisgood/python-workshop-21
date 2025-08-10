4571fd63191992294581481020a478be2dc4e1f0
Miya Shijin Li <miyashijinli@gmail.com>
2025-08-10 20:21:56 +1000

    max_min_calculator_miya

--- work_shop3/miya/max_min_average_calculation_miya.py
+++ work_shop3/miya/max_min_average_calculation_miya.py
@@ -1,18 +1,32 @@
+
 while_check = True
-
 while while_check:
-    number = input("Please enter how many number you going to enter: ")
-
     try:
+        number = input('Please enter how many number you going to enter: ')
         number = int(number)
-        while_check = False #break
+        while_check = False
     except:
-        print("The text you entered is not a number!")
+        print('The text you entered is not a number!')
 
-sum = 0
+values = []
 for i in range(number):
-    value = input("please enter a number: ")
-    sum += float(value) # sum = sum + float(value)
+    value = input('please enter a number: ')
+    values.append(float(value))
 
-print(f"The average number is {sum / number}")
+max_value = max(values)
+min_value = min(values)
+index_max = []
+index_min = []
+
+for i in range(len(values)):
+    if values[i] == max_value:
+        index_max.append(i)
+    if values[i] == min_value:
+        index_min.append(i)
+
+
+print(f'The max number is {max_value} and position at {index_max}')
+print(f'The min number is {min_value} and position at {index_min}')
+print(f'The total is {sum(values)}')
+print(f'The average number is {sum(values) / len(values)}')
 
