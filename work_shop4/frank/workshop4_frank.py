a77f9dbcc05f1a4465a1e0cc6c17e15ee4cbedaf
FrankBai2024 <frankbai2024@gmail.com>
2025-08-10 21:46:09 +1000

    add some git operations

--- work_shop4/frank/workshop4_frank.py
+++ work_shop4/frank/workshop4_frank.py
@@ -6,3 +6,7 @@ print(a)
 print(random.randint(1, 35))
 
 #git chekout -b work_shop4_frank
+#git branch --set-upstream-to=origin/Frank-workshop_4 Frank-workshop_4
+#git pull
+#git commit -m "dfdfdfd"
+#git push
