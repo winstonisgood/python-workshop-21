910baa84cb50584e536daab9b8e65b48c53a84be
FrankBai2024 <frankbai2024@gmail.com>
2025-08-11 20:53:35 +1000

    powerball picking using random.sample to pick unique number

--- work_shop4/frank/workshop4_powerball_frank.py
+++ work_shop4/frank/workshop4_powerball_frank.py
@@ -9,10 +9,34 @@ def picknumbers(poolsize:int, ballnum:int) :
     """
     ball_list =list(range(1, poolsize + 1))#initial and sort list
     random.shuffle(ball_list) #shuffle the list
-    pick_balls = ball_list[:ballnum] #the first 7 numbers in the shuffled list
+    pick_balls = ball_list[:ballnum] #the first numbers in the shuffled list
     return pick_balls
+
+def pickballs(poolsize:int, ballnum:int) -> list:
+    """
+    another function to pick NOT REPEAT numbers from a list.
+    using random.sample to pick unique numbers.
+    """
+    pool_list = list(range(1, poolsize+1))  # Create a list from 1 to poolsize
+    #print("pickballs pool_list:" + str(pool_list))
     
-print(f"pick 7 lucky numbers from 1 to 35:{picknumbers(35, 7)}")
+    picked_list = []  # Initialize an empty list to store picked numbers
+    picked_list = random.sample(pool_list, ballnum)  # Use random.sample to pick unique numbers
+    #print("picked_list:"+str(picked_list))
+    
+    # for i in range(ballnum):
+    #     picked = random.randint(0, len(pool_list) - 1)  # Generate a random index
+    #     picked_list.append(pool_list[picked])  # Append the picked number to the list
+    #     #print("pool_list i="+str(i)+" picked=" +str(picked) +": " + str(pool_list))
+    #     try:
+    #         pool_list.remove(picked)  # Remove the picked number from the pool
+    #     except ValueError:
+    #         print(f"ValueError: {picked} not in pool_list, skipping removal.")
+    #     i += 1
+    return picked_list
+        
+print("pick 7 balls from 1 to 35: " + str(pickballs(35, 7)))        
+#print(f"pick 7 lucky numbers from 1 to 35:{picknumbers(35, 7)}")
 print("the win number is:" + str(picknumbers(20, 1)))
 
 #git chekout -b work_shop4_frank
