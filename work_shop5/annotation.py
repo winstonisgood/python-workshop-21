bafe7c9ca8eb6c2ea6ad15af4d647e6de63e6c11
TiffanyWU-1213 <tingfeiwu44@gmail.com>
2025-08-14 20:56:40 +1000

    tiffany-workshop-5

---
+++ work_shop5/annotation.py
@@ -0,0 +1,4 @@
+def add(a:int,b:int) ->int:
+    return a+b
+
+print("result:", add(a="a",b="b"))
---
+++ work_shop5/read_file.py
@@ -0,0 +1,14 @@
+f=open('example.txt','r')
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+line="start"
+while line is not "":
+    line=f.readline()
+    print(line)
