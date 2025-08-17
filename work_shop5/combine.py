1e0f453c2c440ae220234c121e1138a5685d5fd1
Cassycq <cassycq@gmail.com>
2025-08-17 21:51:01 +1000

    modified workshop5 - Cassy

--- work_shop5/combine.py
+++ work_shop5/combine.py
@@ -1,27 +1,19 @@
 file_name = input("Please enter the file name you want to combine: ")
 try:
-    f1 = open("./data/" + file_name + "-A.txt","rt")
-    f2 = open("./data/" + file_name + "-B.txt","rt")
+    fA = open("./data/" + file_name + "-A.txt","rt")
+    fB = open("./data/" + file_name + "-B.txt","rt")
     filename_out = open(file_name + "-out.txt","at")
 except FileNotFoundError:
     print("File not found")
-line_num_odd = 1
-line_num_even = 1
 while True:
-    odd_line = f1.readline() 
-    if not odd_line:
+    odd_line = fA.readline()     
+    even_line = fB.readline()
+    if not odd_line and not even_line:
         break
-    if line_num_odd %2 ==1:
-        filename_out.write(odd_line)
-    line_num_odd += 1
-    even_line = f2.readline()
-    if not even_line:
-        break
-    if line_num_even %2 ==0:
-        filename_out.write(even_line)
-    line_num_even += 1
-f1.close()
-f2.close()
+    filename_out.write(odd_line)
+    filename_out.write(even_line)   
+fA.close()
+fB.close()
 filename_out.close()
 
 
