811d8f1163bed709a21f545b80581b32ba607e09
TiffanyWU-1213 <tingfeiwu44@gmail.com>
2025-08-17 19:44:21 +1000

    Add workshop4_practice.py

--- work_shop3/tiffany/practice_tiffany.py
+++ work_shop3/tiffany/practice_tiffany.py
@@ -1,3 +1,4 @@
+
 while_check=True
 while while_check:
     try:
@@ -7,24 +8,24 @@ while while_check:
     except:
         print('The text you entered is not a number!')
 
-    values=[]
-    for in range(number):
-        value=input('please enter a number:')
-        value.append(float(value))
+values=[]
+for i in range(number):
+    value=input('please enter a number:')
+    values.append(float(value))
 
-    max_value=max(values)
-    min_value=min(Values)
-    index_max=[]
-    index_min=[]
+max_value=max(values)
+min_value=min(values)
+index_max=[]
+index_min=[]
 
-    for i in range(len(values)):
-        if values[i]==max_value:
+for i in range(len(values)):
+    if values[i]==max_value:
             index_max.append(i)
-        if values[i]==min_value:
+    if values[i]==min_value:
             index_min.append(i)
 
 
-    print(f'The max number is {max(value)} and position at {value.in}')
-    print(f'The max number is {min(value)} and position at {value.in}')
-    print(f'The total is {sum(values)}')
-    print(f'The average number is {sum(values)/len(values)}')
+print(f'The max number is {max(value)} and position at {index_max}')
+print(f'The max number is {min(value)} and position at {index_min}')
+print(f'The total is {sum(values)}')
+print(f'The average number is {sum(values)/len(values)}')
