e207471674bd2a5c80d8b12dab15916607b6fb42
frankbai2024 <frankbai2024@gmail.com>
2025-08-22 10:30:06 +1000

    Update workshop_6_book_frank.py
    1.set attributes as protect;
    2.overwrited display_info;
    3.disinherited inventory from book class

--- work_shop6/frank/workshop_6_book_frank.py
+++ work_shop6/frank/workshop_6_book_frank.py
@@ -1,26 +1,31 @@
 class Book():#a class of book with basic attributes and methods
-    def __init__(self,title, author, isbn, price, stock):
-        self.title = title
-        self.author = author
-        self.isbn = isbn
-        self.price = price
+    def __init__(self, title, author, isbn, price, stock):
+        """attributes are all protected except stock is private for testing"""
+        self._title = title
+        self._author = author
+        self._isbn = isbn
+        self._price = price
         self.__stock = stock
     
     def get_stock(self):
         return self.__stock
     
     def add_stock(self, amount):
+        if amount < 0:
+            return
         self.__stock += amount
     
     def sell_book(self, amount):
+        if amount < 0:
+            return
         if amount <= self.__stock:#compare the amount with stock
             self.__stock -= amount
         else:#if amount is larger than stock, stock will be set to 0
             print(f"only {self.__stock} to sell")
             self.__stock = 0
             
-    def display_info(self):
-        print(f"dispplay info: Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}, Price: {self.price}, Stock: {self.get_stock()}")
+    def display_info(self):#stock is private
+        print(f"dispplay info: Title: {self._title}, Author: {self._author}, ISBN: {self._isbn}, Price: {self._price}, Stock: {self.get_stock()}")
 
 class EBook(Book):
     def __init__(self,title, author, isbn, price, stock,file_format):
@@ -30,56 +35,54 @@ class EBook(Book):
     def set_format(self, file_format):
         self.file_format = file_format
 
-    def ebook_display_info(self):
-        print(f"dispplay info: Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}, Price: {self.price}, Stock: {self.get_stock()}, Format: {self.file_format}")
+    def display_info(self):#overwrite this method
+        print(f"dispplay info: Title: {self._title}, Author: {self._author}, ISBN: {self._isbn}, Price: {self._price}, Stock: {self.get_stock()}, Format: {self.file_format}")
 
 # an inventory class to manage a list of books
 # every element in the list is a Book object
-class BookInventory(Book):
-    #books = []
+class BookInventory():
     def __init__(self, books=[]):
-        #super().__init__(self.title, self.author, self.isbn, self.price, self.stock)
         self.books = books
         
     def add_book(self, book):#to add a new book to the inventory list
         for book_element in self.books:#search if the book already exists in the inventory
-            if book_element.isbn == book.isbn: #isbn is unique for each book
+            if book_element._isbn == book._isbn: #isbn is unique for each book
                 print(f"This book isbn:{book.isbn} already exists in the inventory.")
                 return
         self.books.append(book)#append the book as a element in the inventory list
     
     def remove_book(self, title):#remove this book permenently
         for book in self.books:
-            if book.title == title:# use == to compare the title
+            if book._title == title:# use == to compare the title
                 self.books.remove(book)
                 print(f"Removed book: {title}")
                 return
     
     def add_stock(self, isbn, amount):
         for book in self.books:
-            if book.isbn == isbn:
+            if book._isbn == isbn:
                 book.add_stock(amount)
     
     def sell_book(self, isbn, amount):
         for book in self.books:
-            if book.isbn == isbn:
+            if book._isbn == isbn:
                 book.sell_book(amount)
     
     def display_inventory(self):
         for book in self.books:
-            print(f"Title: {book.title}, inventory: {book.get_stock()}")
+            print(f"Title: {book._title}, inventory: {book.get_stock()}")#stock is private so must be use its method
         
     def find_book(self,title):
         i = 0
         for book in self.books:
-            if title in book.title.lower():
+            if title in book._title.lower():
                 i += 1                      #check value
                 print(f"Found it: ")
-                
-                if isinstance(book, EBook) :#check if it is a ebook
-                    book.ebook_display_info()#print one more attribute:file_format
-                else:                        
-                    book.display_info()#only attributes from parrent class
+                book.display_info()#automatically use the overwrited method if it has a more attribute of file_format
+                # if isinstance(book, EBook) :#check if it is a ebook
+                #     book.ebook_display_info()#print one more attribute:file_format
+                # else:                        
+                #     book.display_info()#only attributes from parrent class
         if i == 0:
             print("Not this book in the inventory")
 
@@ -125,10 +128,10 @@ book_inventory.display_inventory()
 #set a Ebook and change its format attribute
 print("------#set a Ebook and change its format attribute-----------")
 ebook = EBook("harry 1", "au1", "888", 25.5, 30, "pdf")
-ebook.ebook_display_info()
+ebook.display_info()
 print("-------change file_format from pdf to bmp---------------")
 ebook.set_format("bmp")#change format from pdf to bmp
-ebook.ebook_display_info()
+ebook.display_info()
 #append this Ebook into the inventory list
 print("------------#append this Ebook into the inventory list--------")
 book_inventory.add_book(ebook)#add ebook into the inventory list
